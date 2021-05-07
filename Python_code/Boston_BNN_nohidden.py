f461f1021b901cfd7d617c04c2234c04da7a7257
mraabo <magnusraabo@gmail.com>
2021-05-07 10:00:56 +0200

    Changed seed

--- Python_code/Boston_BNN_nohidden.py
+++ Python_code/Boston_BNN_nohidden.py
@@ -1,13 +1,17 @@
 # # ----------------------------- INFO ---------------------------
-# In this python script we implement and run a BNN for predicting house prices 
+# In this python script we implement and run a BNN for predicting house prices
 # in Boston. The sampler is based on the NUTS sampler
 
 # # ----------------------------- IMPORTS ---------------------------
+import warnings
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns
 import sys
 import time
 from keras.datasets import boston_housing
 from sklearn import metrics
-import numpy as np 
+import numpy as np
 import pymc3 as pm
 import theano
 import arviz as az
@@ -16,14 +20,11 @@ import theano.tensor as tt
 Numba.disable_numba()
 Numba.numba_flag
 floatX = theano.config.floatX
-# seaborn for vizualzing 
-import seaborn as sns
+# seaborn for vizualzing
 sns.set_style("white")
 # import pandas as pd
 # from scipy.stats import norm
 # from scipy.stats import t
-import matplotlib.pyplot as plt
-import tensorflow as tf
 # # ----------------------------- Print versions ---------------------------
 
 print("Running on Python version %s" % sys.version)
@@ -33,18 +34,17 @@ print("Running on Arviz version %s" % az.__version__)
 print("Running on Numpy version %s" % np.__version__)
 
 # Ignore warnings - NUTS provide many runtimeWarning
-import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
-tf.random.set_seed(40)
+tf.random.set_seed(42)
 # # ----------------------------- Loading Boston data ---------------------------
 (X_train, y_train), (X_test, y_test) = boston_housing.load_data(seed=3030)
 
 # pad Xs with 1's to add bias
-ones_train=np.ones(X_train.shape[0])
-ones_test=np.ones(X_test.shape[0])
-X_train=np.insert(X_train,0,ones_train,axis=1)
-X_test=np.insert(X_test,0,ones_test,axis=1)
+ones_train = np.ones(X_train.shape[0])
+ones_test = np.ones(X_test.shape[0])
+X_train = np.insert(X_train, 0, ones_train, axis=1)
+X_test = np.insert(X_test, 0, ones_test, axis=1)
 
 #### DO NOT DELETE THIS YET ###
 
@@ -52,8 +52,8 @@ X_test=np.insert(X_test,0,ones_test,axis=1)
 # boston = pd.DataFrame(boston_dataset.data, columns=boston_dataset.feature_names)
 # boston['MEDV'] = boston_dataset.target
 # sns.set(rc={'figure.figsize':(11.7,8.27)})
-# ax = sns.distplot(boston['MEDV'], kde = False, norm_hist=True) 
-# ax = sns.distplot(np.random.normal(loc=22.0, scale=9.0, size=1000000), kde = True, hist=False) 
+# ax = sns.distplot(boston['MEDV'], kde = False, norm_hist=True)
+# ax = sns.distplot(np.random.normal(loc=22.0, scale=9.0, size=1000000), kde = True, hist=False)
 # plt.show()
 # scaler=sc.fit(X_train)
 # X_train = scaler.fit_transform(X_train)
@@ -68,53 +68,56 @@ X_test=np.insert(X_test,0,ones_test,axis=1)
 
 # # ----------------------------- Implementing a BNN function ---------------------------
 
-def construct_bnn(ann_input, ann_output, n_hidden = 5, prior_std = 0.01):
+
+def construct_bnn(ann_input, ann_output, n_hidden=5, prior_std=0.01):
     # Initialize random weights between each layer
-    init_out = np.random.randn(X_train.shape[1],1).astype(floatX)*prior_std
+    init_out = np.random.randn(X_train.shape[1], 1).astype(floatX)*prior_std
 
     with pm.Model() as bayesian_neural_network:
         ann_input = pm.Data("ann_input", X_train)
         ann_output = pm.Data("ann_output", y_train)
-            
+
     # Input layer -> Output layer
         weights_out = pm.Normal('w_out', mu=0, sd=prior_std,
-                            shape=(X_train.shape[1],1),
-                            testval=init_out)
-        acts_out = pm.Deterministic('activations_out',tt.dot(ann_input, weights_out))
-        
+                                shape=(X_train.shape[1], 1),
+                                testval=init_out)
+        acts_out = pm.Deterministic(
+            'activations_out', tt.dot(ann_input, weights_out))
 
-    #Define likelihood
-        out = pm.Normal('out', mu=acts_out[:,0], sd=1, observed=ann_output)        
-            
+    # Define likelihood
+        out = pm.Normal('out', mu=acts_out[:, 0], sd=1, observed=ann_output)
+
     return bayesian_neural_network
 
 
 # # ----------------------------- Sampling from posterior ---------------------------
 
 # Start time
-tic = time.perf_counter() # for timing
-bayesian_neural_network_NUTS = construct_bnn(X_train, y_train, n_hidden=10, prior_std=0.1)
+tic = time.perf_counter()  # for timing
+bayesian_neural_network_NUTS = construct_bnn(
+    X_train, y_train, n_hidden=10, prior_std=0.1)
 
 # Sample from the posterior using the NUTS samplper
 with bayesian_neural_network_NUTS:
-    trace = pm.sample(draws=3000, tune=1000, chains=3,target_accept=.9)
-    
+    trace = pm.sample(draws=3000, tune=1000, chains=3, target_accept=.9)
 
 
 # # ----------------------------- Making predictions on training data ---------------------------
-ppc1=pm.sample_posterior_predictive(trace, model=bayesian_neural_network_NUTS)
+ppc1 = pm.sample_posterior_predictive(
+    trace, model=bayesian_neural_network_NUTS)
 
 # Taking the mean over all samples to generate a prediction
 y_train_pred = ppc1['out'].mean(axis=0)
 
 
 # Replace shared variables with testing set
-pm.set_data(new_data={"ann_input": X_test, "ann_output": y_test}, model=bayesian_neural_network_NUTS)
-
+pm.set_data(new_data={"ann_input": X_test, "ann_output": y_test},
+            model=bayesian_neural_network_NUTS)
 
 
 # # ----------------------------- Making predictions on test data ---------------------------
-ppc2 = pm.sample_posterior_predictive(trace, model=bayesian_neural_network_NUTS)
+ppc2 = pm.sample_posterior_predictive(
+    trace, model=bayesian_neural_network_NUTS)
 
 # Taking the mean over all samples to generate a prediction
 y_test_pred = ppc2['out'].mean(axis=0)
@@ -125,7 +128,8 @@ print(f"Run time {toc - tic:0.4f} seconds")
 
 # Printing the performance measures
 #print('MAE (NUTS) on training data:', metrics.mean_absolute_error(y_train, y_train_pred))
-print('MSE (NUTS) on training data:', metrics.mean_squared_error(y_train, y_train_pred))
+print('MSE (NUTS) on training data:',
+      metrics.mean_squared_error(y_train, y_train_pred))
 #print('MAE (NUTS) on test data:', metrics.mean_absolute_error(y_test, y_test_pred))
 print('MSE (NUTS) on test data:', metrics.mean_squared_error(y_test, y_test_pred))
 
@@ -137,12 +141,12 @@ print('MSE (NUTS) on test data:', metrics.mean_squared_error(y_test, y_test_pred
 
 
 # Vizualize uncertainty by plotting posterior predictive
-ppc2_array=np.array(ppc2['out'])
-plt.hist(ppc2_array[:,40], density=1,color = "skyblue")
+ppc2_array = np.array(ppc2['out'])
+plt.hist(ppc2_array[:, 40], density=1, color="skyblue")
 plt.show()
 
 # # ----------------------------- UNDER CONSTRUCTION ;-) ---------------------------
-### DO NOT DELELTE THIS YET
+# DO NOT DELELTE THIS YET
 # sns.distplot(y_train - pred1)
 # Include a title
 # plt.title("Residuals PDF", size=18)
